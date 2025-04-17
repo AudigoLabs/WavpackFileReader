5f17f7a8c0e070aaa2cafb655c4cbb4211b38be6
Roger Jungemann <rtaljun@gmail.com>
2025-04-17 16:37:33 -0700

    Move Web stuff to its own directory

--- Web/build.sh
+++ Web/build.sh
@@ -6,10 +6,11 @@
 rm -rf build
 mkdir -p build
 # emcc -sEXIT_RUNTIME -I include -I src -I . main.c -o build/index.html --preload-file assets
+sources_path=../Sources/CWavPackFilereader
 emcc -sEXIT_RUNTIME \
-  -I include -I src -I . \
-  -L lib -L src \
-  lib/*.c src/*.c wavpack-js.c \
+  -I $sources_path/include -I $sources_path/src -I $sources_path -I . \
+  -L $sources_path/lib -L $sources_path/src \
+  $sources_path/lib/*.c $sources_path/src/*.c wavpack-js.c \
   -o build/index.html \
   --shell-file index.html \
   -sEXPORTED_FUNCTIONS=_malloc,_read_wavpack_file \
